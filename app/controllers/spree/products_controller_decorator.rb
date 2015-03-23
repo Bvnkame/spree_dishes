c40f7a42a9fb93032ed7f32d33199c430a147d93
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-03-23 09:32:27 +0700

    update

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,3 +1,20 @@
 Spree::Api::ProductsController.class_eval do
 	before_action :authenticate_user, :except => [:index, :show]
+
+	def index
+		if params[:ids]
+			@products = product_scope.where(id: params[:ids].split(",").flatten)
+		else
+			@products = product_scope.ransack(params[:q]).result
+		end
+
+		if params[:date_delivery]
+			@products = @products.where(date_delivery: params[:date_delivery])
+		end 
+		@products = @products.distinct.page(params[:page]).per(params[:per_page])
+		expires_in 15.minutes, :public => true
+		headers['Surrogate-Control'] = "max-age=#{15.minutes}"
+		respond_with(@products)
+	end
+
 end
--- app/views/spree/api/products/small_show.v1.rabl
+++ app/views/spree/api/products/small_show.v1.rabl
@@ -1,7 +1,9 @@
 object @product
 cache [I18n.locale, @current_user_roles.include?('admin'), current_currency, root_object]
 
-attributes :id, :name, :cost_price, :currency
+attributes :id, :name, :date_delivery, :currency
+
+node(:cost_price) { |p| p.cost_price.to_s }
 node(:dish_type){ |p| p.dish_type.name if p.dish_type }
 
 child(:images => :images) { extends "spree/api/images/show" }
