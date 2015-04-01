1f30ced0cc0755aec1bfa5cd715b73cceafa7653
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-04-01 17:01:11 +0700

    udpate

--- app/controllers/spree/api/products_controller_decorator.rb
+++ app/controllers/spree/api/products_controller_decorator.rb
@@ -3,27 +3,26 @@ Spree::Api::ProductsController.class_eval do
 
 	def index
     @products = Spree::Product.all
-		if params[:date_delivery]
-			@products = @products.where(date_delivery: params[:date_delivery])
-    end
-    render "spree/api/products/index", status: 200
-	end
+    if params[:date_delivery]
+     @products = @products.where(date_delivery: params[:date_delivery])
+   end
+   render "spree/api/products/index", status: 200
+ end
 
-   def show
-          @product = find_product(params[:id])
-          expires_in 15.minutes, :public => true
-          headers['Surrogate-Control'] = "max-age=#{15.minutes}"
-          headers['Surrogate-Key'] = "product_id=1"
-           render "spree/api/products/show", status: 200
-        end
+ def show
+  @product = find_product(params[:id])
+  expires_in 15.minutes, :public => true
+  headers['Surrogate-Control'] = "max-age=#{15.minutes}"
+  headers['Surrogate-Key'] = "product_id=1"
+  render "spree/api/products/show", status: 200
+end
 
-	def create
-        authorize! :create, Spree::Product
-        params[:product][:available_on] ||= Time.now
-        set_up_shipping_category
-        p "vao create product"
-        p option_types_params
-        p product_params
+def create
+  authorize! :create, Spree::Product
+  params[:product][:available_on] ||= Time.now
+  set_up_shipping_category
+  p "vao create product"
+  p option_types_params
         # options = { options_attrs: option_types_params }
         # p options
         # @product = Core::Importer::Product.new(nil, product_params, options).create
@@ -35,25 +34,25 @@ Spree::Api::ProductsController.class_eval do
         # p "save ok"
         if @product.persisted?
           @status = [ { "messages" => "Add Product Successful"}]
-            render "spree/api/logger/log"
+          render "spree/api/logger/log"
         else
           invalid_resource!(@product)
         end
       end
 
-       def update
+      def update
         @product = find_product(params[:id])
         authorize! :update, @product
 
-    
-        @product = @product.update(product_params)
+        
+        @product.update(product_params)
 
-        if @product.errors.empty?
+        if @product.update(product_params)
           @status = [ { "messages" => "Update Product Successful"}]
-            render "spree/api/logger/log", status: 201
+          render "spree/api/logger/log", status: 201
         else
           invalid_resource!(@product)
         end
       end
 
-end
+    end
