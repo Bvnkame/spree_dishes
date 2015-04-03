80a2a94ca5a3675ef3850f356782c41bff02febd
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-04-03 10:40:55 +0700

    udpate

--- app/models/dish/ingredient.rb
+++ app/models/dish/ingredient.rb
@@ -3,5 +3,6 @@ module Dish
 		self.table_name = "ingredients"
 		has_many :products, through: :product_ingredients, :class_name => "Spree::Product"
 		has_many :product_ingredients, :class_name => "Dish::ProductIngredient", foreign_key: 'ingredient_id'
+		has_many :images, as: :viewable, dependent: :destroy, class_name: "Dish::IngredientImage"
 	end
 end
