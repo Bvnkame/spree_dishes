3360a3177909efcb705f5c9c0efa85ec2274ed61
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-05-06 14:45:29 +0700

    udpatA

--- app/models/dish/whatneed.rb
+++ app/models/dish/whatneed.rb
@@ -3,6 +3,5 @@ module Dish
 		self.table_name = "whatneeds"
 		has_many :products, through: :product_whatneeds, :class_name => "Spree::Product"
 		has_many :product_whatneeds, :class_name => "Dish::ProductWhatneed", foreign_key: 'whatneed_id'
-		
 	end
 end
