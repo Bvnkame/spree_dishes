b29bbb615263843b35030dd2131d67bc481430a0
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-05-08 11:21:44 +0700

    uapte

--- app/models/spree/spree_product_decorator.rb
+++ app/models/spree/spree_product_decorator.rb
@@ -1,33 +1,30 @@
 Spree::Product.class_eval do 
 
-		# Product.variants.delete
-		has_many :ingredients, through: :product_ingredients, :class_name => "Dish::Ingredient"
-		has_many :product_ingredients, :class_name => "Dish::ProductIngredient", foreign_key: 'product_id'
-		
-		belongs_to :difficulty, :class_name => "Dish::Difficulty", foreign_key: 'difficulty_id'
+	# Product.variants.delete
+	has_many :ingredients, through: :product_ingredients, :class_name => "Dish::Ingredient"
+	has_many :product_ingredients, :class_name => "Dish::ProductIngredient", foreign_key: 'product_id'
+	
+	belongs_to :difficulty, :class_name => "Dish::Difficulty", foreign_key: 'difficulty_id'
 
-		has_many :whatneeds, through: :product_whatneeds, :class_name => "Dish::Whatneed"
-		has_many :product_whatneeds, :class_name => "Dish::ProductWhatneed", foreign_key: 'product_id'
-		
-		has_many :nutritions, through: :product_nutritions, :class_name => "Dish::Nutrition"
-		has_many :product_nutritions, :class_name => "Dish::ProductNutrition", foreign_key: 'product_id'
+	has_many :whatneeds, through: :product_whatneeds, :class_name => "Dish::Whatneed"
+	has_many :product_whatneeds, :class_name => "Dish::ProductWhatneed", foreign_key: 'product_id'
+	
+	has_many :nutritions, through: :product_nutritions, :class_name => "Dish::Nutrition"
+	has_many :product_nutritions, :class_name => "Dish::ProductNutrition", foreign_key: 'product_id'
 
-		has_many :howtocooks, :class_name => "Dish::Howtocook"
+	has_many :howtocooks, :class_name => "Dish::Howtocook"
 
-		has_many :expert, :class_name => "Bm::Expert", foreign_key: 'bm_expert_id'
+	belongs_to :expert, :class_name => "Bm::Expert", foreign_key: 'bm_expert_id'
 
-    has_many :date_deliveries, :class_name => "Dish::DateDelivery"
+	has_many :date_deliveries, :class_name => "Dish::DateDelivery"
 
-    belongs_to :dish_type, :class_name => "Dish::DishType"
+	belongs_to :dish_type, :class_name => "Dish::DishType"
 
 
-    accepts_nested_attributes_for :product_nutritions, allow_destroy: true, reject_if: lambda { |pp| pp[:nutrition_name].blank? }
-  
+	accepts_nested_attributes_for :product_nutritions, allow_destroy: true, reject_if: lambda { |pp| pp[:nutrition_name].blank? }
+	
 	def self.product_of_date(date)
-			Spree::Product.select("spree_products.*, date_deliveries.delivery_date")
-     .joins(:date_deliveries).where(:date_deliveries => {:delivery_date => date }).uniq
-     
-		end
-
-
-  end
+		Spree::Product.select("spree_products.*, date_deliveries.delivery_date")
+		.joins(:date_deliveries).where(:date_deliveries => {:delivery_date => date }).uniq
+	end
+end
