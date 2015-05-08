2ed358a7cba30a334c132b5cfb7fed9c53d80d85
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-05-08 11:31:24 +0700

    udpate

--- app/models/spree/spree_product_decorator.rb
+++ app/models/spree/spree_product_decorator.rb
@@ -14,7 +14,7 @@ Spree::Product.class_eval do
 
 	has_many :howtocooks, :class_name => "Dish::Howtocook"
 
-	belongs_to :expert, :class_name => "Bm::Expert", foreign_key: 'bm_expert_id'
+	has_one :expert_one, :class_name => "Bm::Expert", foreign_key: 'bm_expert_id'
 
 	has_many :date_deliveries, :class_name => "Dish::DateDelivery"
 
@@ -22,7 +22,7 @@ Spree::Product.class_eval do
 
 
 	accepts_nested_attributes_for :product_nutritions, allow_destroy: true, reject_if: lambda { |pp| pp[:nutrition_name].blank? }
-	
+
 	def self.product_of_date(date)
 		Spree::Product.select("spree_products.*, date_deliveries.delivery_date")
 		.joins(:date_deliveries).where(:date_deliveries => {:delivery_date => date }).uniq
